b3c10ad61abbd7e1e500b535fcda547fd35af987
korpikoodari <pasi@korpikoodarit.eu>
2017-11-01 14:46:09 +0200

    Animation added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,42 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+       animations: [
+        trigger('listAnimation', [
+      transition('* => *', [ // each time the binding value changes
+        
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ]),
+    trigger('explainerAnim', [
+      transition('* => *', [
+        query('.welcome_line_col', style({ opacity: 0, transform: 'translateX(-40px)' })),
+
+        query('.welcome_line_col', stagger('500ms', [
+          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+        ])),
+
+        query('.welcome_line_col', [
+          animate(1000, style('*'))
+        ])
+        
+      ])
+    ])
+  ]
+  
 })
 export class HomeComponent implements OnInit {
   
